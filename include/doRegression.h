c0428b8aeb7560f1e7066cb703d4021b8797992e
zizowasfy <abdelazizshaarawy@gmail.com>
2023-08-23 21:37:39 +0100

    Save samples in rosbag
    and a failed attempt to run doRegression from a ros node rather that .h file

--- include/doRegression.h
+++ include/doRegression.h
@@ -76,6 +76,26 @@ void getGMMfromBag(Eigen::MatrixXf &GMM_X, std::vector<Eigen::VectorXf> &GMM_mea
     }
   }
   rbag.close();
+
+  rosbag::Bag wbag;
+  wbag.open("/home/zizo/Disassembly Teleop/LL/Rcover/gmm-gmr/GMMsamples.bag", rosbag::bagmode::Write);
+  wbag.close();
+}
+
+// Save samples to rosbag for later visualize
+void saveSamples(std::vector<double> & sample_pnt)
+{
+  rosbag::Bag wbag;
+  wbag.open("/home/zizo/Disassembly Teleop/LL/Rcover/gmm-gmr/GMMsamples.bag", rosbag::bagmode::Append);
+
+  geometry_msgs::PoseStamped sample;
+  sample.header.frame_id = "panda_link0";
+  sample.pose.position.x = sample_pnt[0];
+  sample.pose.position.y = sample_pnt[1];
+  sample.pose.position.z = sample_pnt[2];
+
+  wbag.write(("/samples"), ros::Time::now(), sample);
+  wbag.close();
 }
 
 // this function compute PDF of X which has lenght of k
@@ -97,83 +117,83 @@ float computeNormalDistributionPDF(Eigen::VectorXf X, Eigen::VectorXf mean, Eige
 }
 
 std::vector<float> doRegression(float input, Eigen::MatrixXf  xi, const std::vector<Eigen::VectorXf>  means, const std::vector<float>  weights , const std::vector<Eigen::MatrixXf>  covariances)
+{
+  std::cout << "    *** Starting Regression ***    " << std::endl;
+  
+  int numTrajsamples = 1;     // number of points in the chosen trajectory
+  int i = 0;
+
+  Eigen::VectorXf X;
+  Eigen::VectorXf mean;
+  Eigen::MatrixXf covariance;
+
+  Eigen::MatrixXf out_xi;
+
+  out_xi.resize(numTrajsamples, xi.cols());
+  out_xi=Eigen::MatrixXf::Zero(numTrajsamples, xi.cols());
+
+//equation 10
+  float xi_hat_s_k, mu_s_k,mu_t_k,sigma_st_k,inv_sigma_t_k,sigma_hat_s_k,
+        sigma_s_k, sigma_t_k,sigma_ts_k, xi_t, beta_k,xi_hat_s,beta_k_sum, beta_k_xi_hat_s_k_sum;
+
+  for(std::size_t coord=1; coord<out_xi.cols(); coord++)    // loop over the xyz coordinates (dimenstions of the model)
   {
-    std::cout << "    *** Starting Regression ***    " << std::endl;
-    
-    int numTrajsamples = 1;     // number of points in the chosen trajectory
-    int i = 0;
-
-    Eigen::VectorXf X;
-    Eigen::VectorXf mean;
-    Eigen::MatrixXf covariance;
-
-    Eigen::MatrixXf out_xi;
-
-    out_xi.resize(numTrajsamples, xi.cols());
-    out_xi=Eigen::MatrixXf::Zero(numTrajsamples, xi.cols());
-
-  //equation 10
-    float xi_hat_s_k, mu_s_k,mu_t_k,sigma_st_k,inv_sigma_t_k,sigma_hat_s_k,
-          sigma_s_k, sigma_t_k,sigma_ts_k, xi_t, beta_k,xi_hat_s,beta_k_sum, beta_k_xi_hat_s_k_sum;
-
-    for(std::size_t coord=1; coord<out_xi.cols(); coord++)    // loop over the xyz coordinates (dimenstions of the model)
+    beta_k_sum=0;
+    beta_k_xi_hat_s_k_sum=0;
+    for(std::size_t k=0; k<means.size(); k++)     // loop over the number of gaussians
     {
-      beta_k_sum=0;
-      beta_k_xi_hat_s_k_sum=0;
-      for(std::size_t k=0; k<means.size(); k++)     // loop over the number of gaussians
-      {
-        mu_t_k=means.at(k)(0);
-        mu_s_k=means.at(k)(coord);
-        sigma_st_k=covariances.at(k)(coord,0);
-        sigma_t_k=covariances.at(k)(0,0);
-        inv_sigma_t_k=1/sigma_t_k;
-        sigma_s_k=covariances.at(k)(coord,coord);
-        sigma_ts_k=covariances.at(k)(0,coord);
-        // xi_t=xi(i,0);
-        // xi_t = i;
-        // xi_t = sqrt(pow(mp->O_T_EE_c[12],2) + pow(mp->O_T_EE_c[13],2) + pow(mp->O_T_EE_c[14],2));
-        xi_t = input; //pose_regress;
-        // xi_t = xi(i,3);
-        xi_hat_s_k=mu_s_k+ sigma_st_k*inv_sigma_t_k*(xi_t -mu_t_k);
-        sigma_hat_s_k=sigma_s_k -sigma_st_k*inv_sigma_t_k*sigma_ts_k;
+      mu_t_k=means.at(k)(0);
+      mu_s_k=means.at(k)(coord);
+      sigma_st_k=covariances.at(k)(coord,0);
+      sigma_t_k=covariances.at(k)(0,0);
+      inv_sigma_t_k=1/sigma_t_k;
+      sigma_s_k=covariances.at(k)(coord,coord);
+      sigma_ts_k=covariances.at(k)(0,coord);
+      // xi_t=xi(i,0);
+      // xi_t = i;
+      // xi_t = sqrt(pow(mp->O_T_EE_c[12],2) + pow(mp->O_T_EE_c[13],2) + pow(mp->O_T_EE_c[14],2));
+      xi_t = input; //pose_regress;
+      // xi_t = xi(i,3);
+      xi_hat_s_k=mu_s_k+ sigma_st_k*inv_sigma_t_k*(xi_t -mu_t_k);
+      sigma_hat_s_k=sigma_s_k -sigma_st_k*inv_sigma_t_k*sigma_ts_k;
 
 
-        //equation 11
+      //equation 11
 
-        X.resize(1,1);
-        mean.resize(1,1);
-        covariance.resize(1,1);
+      X.resize(1,1);
+      mean.resize(1,1);
+      covariance.resize(1,1);
 
 
 
-        X<<xi_t;
-        mean<<mu_t_k;
+      X<<xi_t;
+      mean<<mu_t_k;
 
-        covariance<<sigma_t_k;
+      covariance<<sigma_t_k;
 
 
 
 
-        beta_k=weights.at(k) * computeNormalDistributionPDF(X, mean, covariance);
-        beta_k_sum=beta_k_sum+beta_k;
+      beta_k=weights.at(k) * computeNormalDistributionPDF(X, mean, covariance);
+      beta_k_sum=beta_k_sum+beta_k;
 
-        xi_hat_s=beta_k*xi_hat_s_k;
-        beta_k_xi_hat_s_k_sum=beta_k_xi_hat_s_k_sum+xi_hat_s;
-      }
-      out_xi(0,0)=xi_t;
-      out_xi(0,coord)=beta_k_xi_hat_s_k_sum/beta_k_sum;
+      xi_hat_s=beta_k*xi_hat_s_k;
+      beta_k_xi_hat_s_k_sum=beta_k_xi_hat_s_k_sum+xi_hat_s;
     }
-    
-    std::cout<< "-------------------------------------" <<std::endl;
-
-    std::cout<< "out_xi:" <<std::endl <<out_xi <<std::endl;
-
-
-    std::cout<< "out_xi.rows():"  <<out_xi.rows() <<std::endl;
-    std::cout<< "out_xi.cols():"  <<out_xi.cols() <<std::endl;
-
-    return {out_xi(0,1), out_xi(0,2), out_xi(0,3)};
+    out_xi(0,0)=xi_t;
+    out_xi(0,coord)=beta_k_xi_hat_s_k_sum/beta_k_sum;
   }
+  
+  std::cout<< "-------------------------------------" <<std::endl;
+
+  std::cout<< "out_xi:" <<std::endl <<out_xi <<std::endl;
+
+
+  std::cout<< "out_xi.rows():"  <<out_xi.rows() <<std::endl;
+  std::cout<< "out_xi.cols():"  <<out_xi.cols() <<std::endl;
+
+  return {out_xi(0,1), out_xi(0,2), out_xi(0,3)};
+}
 
 // THIS FUNCTION IS SUPPOSED TO BE IN model_based_planning_context.cpp, but I put it here as a backup
 // ompl::base::ValidStateSamplerPtr
--- src/gmm_node.cpp
+++ src/gmm_node.cpp
@@ -64,7 +64,7 @@ using namespace boost::filesystem;
 // srv
 #include <data_handle/DemonsInfo.h>
 #include <gaussian_mixture_model/DoRegression.h>
-
+// #include <gaussian_mixture_model/gmm_node.hpp>
 class GMMNode
 {
   public:
@@ -460,6 +460,135 @@ class GMMNode
     std::cout<< "out_xi.rows():"  <<out_xi.rows() <<std::endl;
     std::cout<< "out_xi.cols():"  <<out_xi.cols() <<std::endl;
 
+  }
+
+  std::vector<float> doRegression(float input, Eigen::MatrixXf  xi, const std::vector<Eigen::VectorXf>  means, const std::vector<float>  weights , const std::vector<Eigen::MatrixXf>  covariances)
+  {
+      std::cout << "    *** Starting Regression ***    " << std::endl;
+      
+      int numTrajsamples = 1;     // number of points in the chosen trajectory
+      int i = 0;
+
+      Eigen::VectorXf X;
+      Eigen::VectorXf mean;
+      Eigen::MatrixXf covariance;
+
+      Eigen::MatrixXf out_xi;
+
+      out_xi.resize(numTrajsamples, xi.cols());
+      out_xi=Eigen::MatrixXf::Zero(numTrajsamples, xi.cols());
+
+    //equation 10
+      float xi_hat_s_k, mu_s_k,mu_t_k,sigma_st_k,inv_sigma_t_k,sigma_hat_s_k,
+            sigma_s_k, sigma_t_k,sigma_ts_k, xi_t, beta_k,xi_hat_s,beta_k_sum, beta_k_xi_hat_s_k_sum;
+
+      for(std::size_t coord=1; coord<out_xi.cols(); coord++)    // loop over the xyz coordinates (dimenstions of the model)
+      {
+        beta_k_sum=0;
+        beta_k_xi_hat_s_k_sum=0;
+        for(std::size_t k=0; k<means.size(); k++)     // loop over the number of gaussians
+        {
+          mu_t_k=means.at(k)(0);
+          mu_s_k=means.at(k)(coord);
+          sigma_st_k=covariances.at(k)(coord,0);
+          sigma_t_k=covariances.at(k)(0,0);
+          inv_sigma_t_k=1/sigma_t_k;
+          sigma_s_k=covariances.at(k)(coord,coord);
+          sigma_ts_k=covariances.at(k)(0,coord);
+          // xi_t=xi(i,0);
+          // xi_t = i;
+          // xi_t = sqrt(pow(mp->O_T_EE_c[12],2) + pow(mp->O_T_EE_c[13],2) + pow(mp->O_T_EE_c[14],2));
+          xi_t = input; //pose_regress;
+          // xi_t = xi(i,3);
+          xi_hat_s_k=mu_s_k+ sigma_st_k*inv_sigma_t_k*(xi_t -mu_t_k);
+          sigma_hat_s_k=sigma_s_k -sigma_st_k*inv_sigma_t_k*sigma_ts_k;
+
+
+          //equation 11
+
+          X.resize(1,1);
+          mean.resize(1,1);
+          covariance.resize(1,1);
+
+
+
+          X<<xi_t;
+          mean<<mu_t_k;
+
+          covariance<<sigma_t_k;
+
+
+
+
+          beta_k=weights.at(k) * computeNormalDistributionPDF(X, mean, covariance);
+          beta_k_sum=beta_k_sum+beta_k;
+
+          xi_hat_s=beta_k*xi_hat_s_k;
+          beta_k_xi_hat_s_k_sum=beta_k_xi_hat_s_k_sum+xi_hat_s;
+        }
+        out_xi(0,0)=xi_t;
+        out_xi(0,coord)=beta_k_xi_hat_s_k_sum/beta_k_sum;
+      }
+      
+      std::cout<< "-------------------------------------" <<std::endl;
+
+      std::cout<< "out_xi:" <<std::endl <<out_xi <<std::endl;
+
+
+      std::cout<< "out_xi.rows():"  <<out_xi.rows() <<std::endl;
+      std::cout<< "out_xi.cols():"  <<out_xi.cols() <<std::endl;
+
+      return {out_xi(0,1), out_xi(0,2), out_xi(0,3)};
+  }
+
+  void getGMMfromBag(Eigen::MatrixXf &GMM_X, std::vector<Eigen::VectorXf> &GMM_means, std::vector<float> &GMM_weights, std::vector<Eigen::MatrixXf> &GMM_covariances)
+  {
+    // Getting the GMM from the .bag file
+    rosbag::Bag rbag;
+    rbag.open("/home/zizo/Disassembly Teleop/LL/Rcover/gmm-gmr/gmm_mix_action2.bag");
+
+    // // Initializing Variables
+    // Eigen::MatrixXf GMM_X;
+    // std::vector<Eigen::VectorXf> GMM_means;
+    // std::vector<Eigen::MatrixXf> GMM_covariances;
+    // std::vector<float> GMM_weights;
+
+    for(rosbag::MessageInstance const m: rosbag::View(rbag))
+    {
+      gaussian_mixture_model::GaussianMixture::ConstPtr i = m.instantiate<gaussian_mixture_model::GaussianMixture>();
+      if (i != nullptr)
+      {
+        int GMM_dim = sizeof(i->gaussians[0].means)/3; // 3 cause it seems the size of float32 is 3 bytes instead of 4 bytes!!
+        ROS_INFO("GMM Dimension: %d", GMM_dim);
+
+        // Read GaussianMixutre msg and convert it to a format that doRegression(....) accepts
+        GMM_X = Eigen::MatrixXf::Identity(1,GMM_dim); // Not really necessary, it's just only to abide by doRegression(....) format
+        GMM_means.clear();
+        GMM_covariances.clear();
+        Eigen::VectorXf G_means(GMM_dim);
+        Eigen::MatrixXf G_covariances(GMM_dim, GMM_dim);      
+        GMM_weights = i->weights;
+        for (gaussian_mixture_model::Gaussian gaussian : i->gaussians)
+        {
+          // Eigen::VectorXf G_means(GMM_dim);
+          // Eigen::MatrixXf G_covariances(GMM_dim, GMM_dim);
+          int i = 0;
+          for (int d = 0; d < GMM_dim; d++)
+          {
+            G_means(d) = gaussian.means[d];
+
+            for (int c = 0; c < GMM_dim; c++)
+            {
+              G_covariances(d,c) = gaussian.covariances[i]; //[c+i]
+            }
+            i++; //=+ GMM_dim; // To make sure that each row from gaussian.coavariances is placed correctly in G_covariances
+          }
+          GMM_means.push_back(G_means);
+          GMM_covariances.push_back(G_covariances);
+        }
+      }
+    }
+    rbag.close();
   }
 
   // Regress over the poseError of the robot's current pose
